d790a88c3692533fc6622fa2b446dde923faa391
unknown <mihai.badi@yahoo.com>
2021-11-18 21:47:29 +0200

    Task 1

--- app/src/main/java/com/upt/cti/smartwallet/MainActivity.java
+++ app/src/main/java/com/upt/cti/smartwallet/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.upt.cti.model.MonthlyExpenses;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -27,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView tStatus;
     private EditText eSearch, eIncome, eExpenses;
     private Button bSearch, bUpdate;
+    private Spinner sSearch;
+
     // firebase
     private DatabaseReference databaseReference;
 
@@ -42,14 +48,51 @@ public class MainActivity extends AppCompatActivity {
         eSearch = (EditText) findViewById(R.id.eSearch);
         eIncome = (EditText) findViewById(R.id.eIncome);
         eExpenses = (EditText) findViewById(R.id.eExpenses);
+        sSearch = (Spinner) findViewById(R.id.spinner);
 
         bSearch = (Button) findViewById(R.id.bSearch);
         bUpdate = (Button) findViewById(R.id.bUpdate);
 
+        // data structures
+        final List<MonthlyExpenses> monthlyExpenses = new ArrayList<>();
+        final List<String> monthNames = new ArrayList<>();
+
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://smart-wallet-3b5c7-default-rtdb.europe-west1.firebasedatabase.app/");
         databaseReference = database.getReference();
 
+        // spinner adapter
+        final ArrayAdapter<String> sAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, monthNames);
+        sAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sSearch.setAdapter(sAdapter);
+        databaseReference.child("calendar").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot monthSnapshot : dataSnapshot.getChildren()) {
+
+                    // create a new instance of MonthlyExpense
+                    MonthlyExpenses monthlyExpensesSpanshot = dataSnapshot.getValue(MonthlyExpenses.class);
+                    if(checkNames(monthNames,monthSnapshot.getKey())==0)
+                        monthNames.add(monthSnapshot.getKey());
+
+                }
+                if(currentMonth!=null)
+                {
+                    sSearch.setSelection(monthNames.indexOf(currentMonth));
+                }
+
+                // notify the spinner that data may have changed
+                sAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
+
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void clicked(View view) {
@@ -118,5 +161,14 @@ public class MainActivity extends AppCompatActivity {
         databaseReference.child("calendar").child(currentMonth).child("income").setValue(income);
         tStatus.setText("Done");
 
+    }
+
+    public int checkNames(List<String> monthNames, String key) {
+        for (String it : monthNames)
+        {
+            if(it==key)
+                return 1;
+        }
+        return 0;
     }
 }
